88a521fde22690bcc93875523ad72a7d2a939b20
Igor Nikolic <igornikolic77@yahoo.com>
2019-02-09 22:45:20 +0100

    Update nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,5 @@
 <div class="container justify-content-center">
-    <a class="navbar-brand" href="{{ route('shop') }}">
+    <a class="navbar-brand" href="{{ route('shop') }}" onclick="event.preventDefault(); document.getElementById('change-shop-form').submit();">
         ThinkIT - Shop
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
